603e02afb62b8bd92bb81a276441e44cfb6e1db2
Ewin1802 <elantapa@gmail.com>
2024-10-02 02:21:24 +0800

    edit seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,47 +16,12 @@ class UserSeeder extends Seeder
         \App\Models\User::create([
             'name' => 'EwinLantapa (Admin)',
             'email' => 'ewin@fic10.com',
-            'password' => Hash::make('11111111'),
+            'password' => Hash::make('Pass310317'),
             'roles' => 'ADMIN',
             'phone' => '081340985993',
             'skpd' => 'AllSKPD',
         ]);
 
-        \App\Models\User::create([
-            'name' => 'User (BADAN PENGELOLA KEUANGAN)',
-            'email' => 'ewin@fic11.com',
-            'password' => Hash::make('11111111'),
-            'roles' => 'SKPD',
-            'phone' => '081340985993',
-            'skpd' => 'BADAN PENGELOLAAN KEUANGAN DAN PENDAPATAN DAERAH',
-        ]);
-
-        \App\Models\User::create([
-            'name' => 'User (BAPELITBANGDA)',
-            'email' => 'ewin@fic12.com',
-            'password' => Hash::make('11111111'),
-            'roles' => 'SKPD',
-            'phone' => '081340985993',
-            'skpd' => 'BADAN PERENCANAAN PEMBANGUNANAN PENELITIAN DAN PENGEMBANGAN DAERAH',
-        ]);
-
-        \App\Models\User::create([
-            'name' => 'User (RSUD PRATAMA)',
-            'email' => 'ewin@fic13.com',
-            'password' => Hash::make('11111111'),
-            'roles' => 'SKPD',
-            'phone' => '081340985993',
-            'skpd' => 'DINAS PENDIDIKAN DAN KEBUDAYAAN',
-        ]);
-
-        \App\Models\User::create([
-            'name' => 'Pimpinan BPKPD',
-            'email' => 'ewin@fic14.com',
-            'password' => Hash::make('11111111'),
-            'roles' => 'KABAN',
-            'phone' => '081340985993',
-            'skpd' => 'KABAN',
-        ]);
 
     }
 }
